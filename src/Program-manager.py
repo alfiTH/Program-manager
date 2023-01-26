9069e4a3382d573d7f06a32d0d1df9123113dd97
alfiTH <alex_alfi@hotmail.com>
2023-01-26 13:07:06 +0100

    no update before commit

--- src/Program-manager.py
+++ src/Program-manager.py
@@ -3,6 +3,8 @@ import os, signal
 import time
 from threading import Thread
 from PySide6 import QtCore, QtWidgets, QtGui
+from multiprocessing import Process
+import subprocess
 
 def handler(signum, frame):
     signame = signal.Signals(signum).name
@@ -16,37 +18,39 @@ signal.signal(signal.SIGINT, handler)
 
 
 class myButon(QtWidgets.QPushButton):
-    def __init__(self,row=0, col=0, function=os.system, argument="echo Vacio", parent=None):
+    def __init__(self,row=0, col=0, ruta="/", argument=["echo", "Vacio"], config="", parent=None):
         super(myButon, self).__init__(parent)
         self.row = row
         self.col = col
-        self.function = function
+        self.ruta = ruta
         self.argument = argument
+        self.config = config
+        self.process = None
+
+    def __del__(self):
+        print('Destructor called, Employee deleted.')
+        if self.process != None: 
+            print ( "pid: ", self.process.poll())
+            self.process.kill()
     
-    def launch_function(self):
+    def launch_program(self):
+        self.process = subprocess.Popen(universal_newlines=True, stdout=subprocess.PIPE , cwd="/home/alfith/UNEX/Laboratorio/Program-manager/tmp",args=self.argument + [self.config])
+        #os.set_blocking(self.process.stdout.fileno(), False)
+        print ( "pid: ", self.process.pid)
+        child = Thread(target=self.monitor_program, daemon=True)
+        child.start()
 
-        child_pid = os.fork()
-
-        #padre
-        if child_pid > 0:
-            pid, status = os.waitpid(child_pid, 0)
-            print ("wait returned, pid = %d, status = %d" % (pid, status))
-            pid, status = os.wait()
-            print ("wait returned, pid = %d, status = %d" % (pid, status))
-            
-            '''
-            if (result == 0) {
-            // Child still alive
-            } else if (result == -1) {
-            // Error 
-            } else {
-            // Child exited
-            }'''
-            time.sleep(0.5)
-        #hijo
-        else:
-            self.function(self.argument)
-            os._exit(0)
+    def monitor_program(self):
+        print("monitoring")
+        aux = None
+        a = 0
+        while aux==None:
+            aux = self.process.stdout.readline()
+            if len(aux)>0: print("esta term", aux)
+            aux = self.process.poll()
+            print("program ", self.process.args, " valor ", aux," contador", a)
+            a+=1
+        print("no leido", self.process.stdout.read())
 
 
 class MyWidget(QtWidgets.QWidget):
@@ -59,7 +63,10 @@ class MyWidget(QtWidgets.QWidget):
         data1 = ['hola','hola2','calculator']
         data2 = ['1.3.4.','2.05.5.5.','3.051','35.6.7.9999']
         filas=4
-        program = ["python3 ../tmp/hola.py", "python3 ../tmp/hola2.py", "python3 ../tmp/calculator.py"]
+
+        program = [["python3" ,"hola.py"], 
+            ["python3" ,"calculator.py"], 
+            ["./cc.o"]]
 
         #Ajuste de columnas a estrechas
         header = self.table.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.ResizeMode().ResizeToContents) 
@@ -75,7 +82,7 @@ class MyWidget(QtWidgets.QWidget):
             self.table.setItem(index,0,item1)
             item2 = QtWidgets.QTableWidgetItem(data2[index])
             self.table.setItem(index,1,item2)
-            btn_sell = myButon('run', argument=program[index])
+            btn_sell = myButon('run', argument=program[index], config="ala")
 
             item_color = QtWidgets.QTableWidgetItem()
             item_color.setBackground(QtGui.QColor.fromRgb(0, 255, 0))
@@ -89,22 +96,28 @@ class MyWidget(QtWidgets.QWidget):
     def handleButtonClicked(self):
         button = self.sender()
         print(button.button_row, "x", button.button_column)
-        print (button.launch_function())
+        print (button.launch_program())
 
-def launch_IU():
+if __name__ == "__main__":
+
     app = QtWidgets.QApplication([])
     widget = MyWidget()
     widget.resize(1100, 600)
     widget.show()
-    app.exec()
-
-if __name__ == "__main__":
-    widget = None
-
-    t = Thread(target=launch_IU, args=[])
-
-    t.start()
-
-    t.join()
     
-    sys.exit()
+    sys.exit(app.exec())
+
+
+
+#     from subprocess import Popen, PIPE
+
+
+# def kill(pid, passwd):
+#     pipe = Popen(['sudo', '-S', 'kill', '-9', str(pid)], 
+#                  stdout=PIPE, 
+#                  stdin=PIPE, 
+#                  stderr=PIPE)
+#     pipe.stdin.write(bytes(passwd + '\n', encoding='utf-8'))
+#     pipe.stdin.flush()
+#     # at this point, the process is killed, return output and errors
+#     return (str(pipe.stdout.read()), str(pipe.stderr.read()))
